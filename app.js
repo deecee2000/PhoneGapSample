97e703e962492c6a2a7e86ea4846485f06ded61e
dgajera <dgajera@ebay.com>
2013-07-24 17:16:32 -0700

    Added10

--- app.js
+++ app.js
@@ -5,24 +5,45 @@ window.addEventListener('load', function () {
 // The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
 var homePage =
     '<div>' +
-        '<div class="header"><h1>Page Slider</h1></div>' +
+        '<div class="header"><h1>WatchIt</h1></div>' +
         '<div class="scroller">' +
             '<ul class="list">' +
-                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
-                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
-                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
+                '<li><div style="padding:20px;border-right:1px solid #7D7D7D;border-bottom:5px solid #B1CA39;"><a href="#page1"><strong>GALLERY</strong></a></div></li>' +
+                '<li><div style="padding:20px;border-right:1px solid #7D7D7D;border-bottom:5px solid #CCCCCC"><a href="#page2"><strong>MAP</strong></div></a></li>' +
+                '<li><div style="padding:20px;border-right:0px solid;border-bottom:5px solid #CCCCCC"><a href="#page3"><strong>TAKE PICTURE</strong></div></a></li>' +
             '</ul>' +
+            '<div class="robot">' +
+            	'Gallery Page Contents' +
+            '</div>' +
         '</div>' +
     '</div>';
 
-var detailsPage =
+var mapPage =
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><h1>WatchIt</h1></div>' +
         '<div class="scroller">' +
+		    '<ul class="list">' +
+		        '<li><div style="padding:20px;border-right:1px solid #7D7D7D;border-bottom:5px solid #CCCCCC;"><a href="#page1"><strong>GALLERY</strong></a></div></li>' +
+		        '<li><div style="padding:20px;border-right:1px solid #7D7D7D;border-bottom:5px solid #B1CA39"><a href="#page2"><strong>MAP</strong></div></a></li>' +
+		        '<li><div style="padding:20px;border-right:0px solid;border-bottom:5px solid #CCCCCC"><a href="#page3"><strong>TAKE PICTURE</strong></div></a></li>' +
+		    '</ul>' +
             '<div class="robot">' +
-                '<img src="images/{{img}}"/>' +
-                '<h2>{{name}}</h2>' +
-                '<p>{{description}}</p>' +
+                'MAP Page Contents' +
+            '</div>' +
+        '</div>' +
+    '</div>';
+
+var takePicturePage =
+    '<div>' +
+        '<div class="header"><h1>WatchIt</h1></div>' +
+        '<div class="scroller">' +
+		    '<ul class="list">' +
+		        '<li><div style="padding:20px;border-right:1px solid #7D7D7D;border-bottom:5px solid #CCCCCC;"><a href="#page1"><strong>GALLERY</strong></a></div></li>' +
+		        '<li><div style="padding:20px;border-right:1px solid #7D7D7D;border-bottom:5px solid #CCCCCC"><a href="#page2"><strong>MAP</strong></div></a></li>' +
+		        '<li><div style="padding:20px;border-right:0px solid;border-bottom:5px solid #B1CA39"><a href="#page3"><strong>TAKE PICTURE</strong></div></a></li>' +
+		    '</ul>' +
+            '<div class="robot">' +
+                'Take Picture Page Contents' +
             '</div>' +
         '</div>' +
     '</div>';
@@ -37,13 +58,16 @@ function route(event) {
         hash = window.location.hash;
 
     if (hash === "#page1") {
-        page = merge(detailsPage, {img: "buildbot.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+    	page = homePage;
+        //page = merge(detailsPage, {img: "buildbot.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
     } else if (hash === "#page2") {
-        page = merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        //page = merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+    	page = mapPage;
 //        slider.slide($(page), "right");
     } else if (hash === "#page3") {
-        page = merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+    	page = takePicturePage;
+        //page = merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
     }
     else {
